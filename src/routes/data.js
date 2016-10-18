46d371ee4f65154fad7bd9dbd78346eac3c9865e
alvarollmenezes <alvarollmenezes@gmail.com>
2016-10-18 11:59:11 -0200

    Renomeia os endpoints de storage

--- src/routes/data.js
+++ src/routes/data.js
@@ -4,12 +4,16 @@ module.exports = app => {
 
     app.post( '/data/favoriteBusLines', dataController.saveFavoriteBusLines );
     app.get( '/data/favoriteBusLines', dataController.getFavoriteBusLines );
+    app.post( '/ceturb/data/favorite', dataController.saveFavoriteBusLines );
+    app.get( '/ceturb/data/favorite', dataController.getFavoriteBusLines );
 
     app.post( '/data/settings', dataController.saveSettings );
     app.get( '/data/settings', dataController.getSettings );
 
     app.post( '/data/vehicles', dataController.saveVehicles );
     app.get( '/data/vehicles', dataController.getVehicles );
+    app.post( '/detran/data/vehicles', dataController.saveVehicles );
+    app.get( '/detran/data/vehicles', dataController.getVehicles );
 
     return app;
 };
