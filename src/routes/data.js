36983e5f7799fbd42c502239df5a1bf381baea03
alvarollmenezes <alvarollmenezes@gmail.com>
2017-05-04 18:41:24 -0300

    Altera rota de favoritos do TranscolOnline

--- src/routes/data.js
+++ src/routes/data.js
@@ -7,8 +7,8 @@ module.exports = app => {
     app.post( '/ceturb/data/favorite', dataController.saveFavoriteBusLines );
     app.get( '/ceturb/data/favorite', dataController.getFavoriteBusLines );
 
-    app.post( '/ceturb/buscaBus/data/favorite', dataController.saveFavoriteBuscaBus );
-    app.get( '/ceturb/buscaBus/data/favorite', dataController.getFavoriteBuscaBus );
+    app.post( '/ceturb/transcolOnline/data/favoriteStops', dataController.saveFavoriteBuscaBus );
+    app.get( '/ceturb/transcolOnline/data/favoriteStops', dataController.getFavoriteBuscaBus );
 
     app.post( '/data/settings', dataController.saveSettings );
     app.get( '/data/settings', dataController.getSettings );
