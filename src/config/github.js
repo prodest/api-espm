215f8be8b9e077a301705a82e8525e3f62359e8c
vizeke <vizeke@gmail.com>
2018-12-20 15:07:51 -0200

    refactor(app): github access token env variable name

--- src/config/github.js
+++ src/config/github.js
@@ -1,6 +1,6 @@
 const membersUri = process.env.MEMBERS_URI || 'https://api.github.com/teams/1973692/members';
-const accessToken = process.env.ACCESS_TOKEN || '';
+const githubAccessToken = process.env.GITHUB_ACCESS_TOKEN || '';
 
 module.exports = {
-    espmTeamMembers: membersUri + '?access_token=' + accessToken
+    espmTeamMembers: membersUri + '?access_token=' + githubAccessToken
 };
